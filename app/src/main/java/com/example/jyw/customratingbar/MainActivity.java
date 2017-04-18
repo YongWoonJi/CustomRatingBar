323ffbb6b1c99c7d1c3f13d6a98953fe113ba4f2
Yongwoon Ji <yongwoon@skoopmedia.co.kr>
2017-04-19 00:29:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jyw/customratingbar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.jyw.customratingbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                int rate = (int) Math.ceil(rating);
+//                int rate = (int) rating;
+                if (rate == 0) {
+                    rate = 1;
+                }
+                ratingBar.setRating(rate);
+                textView.setText("" + ratingBar.getRating());
+            }
+        });
+
+    }
+}
